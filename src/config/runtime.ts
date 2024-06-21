d09bd483d50337e60cc32cd6ef9e6da6889fa522
Jakub Parapura <jakubparapura@gmail.com>
2024-06-21 22:49:56 +0200

    :wrench: Add dotenv

--- src/config/runtime.ts
+++ src/config/runtime.ts
@@ -1,2 +1,21 @@
-export const UPDATE_INTERVAL = 2;
-export const IAM_ALIVE_MESSAGE_INTERVAL = 10;
+import requireEnv from 'require-environment-variables';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const loadConfig = () => {
+  const env = requireEnv(['UPDATE_INTERVAL', 'IAM_ALIVE_MESSAGE_INTERVAL']);
+
+  return {
+    secrets: {
+      // intentionally left blank
+    },
+    timing: {
+      updateInterval: env.UPDATE_INTERVAL,
+      iamAliveMessageInterval: env.IAM_ALIVE_MESSAGE_INTERVAL,
+    },
+  };
+};
+
+const config = loadConfig();
+export default config;
--- src/selfNotify/index.ts
+++ src/selfNotify/index.ts
@@ -1,7 +1,10 @@
-import { logInfo } from "../basicService/logging/applicationLogging";
-import { IAM_ALIVE_MESSAGE_INTERVAL } from "../config/runtime";
+import { logInfo } from '../basicService/logging/applicationLogging';
+import config from '../config/runtime';
 
 export const sendIamAliveMessages = () => {
-  logInfo('[SERVICE HEALTH] Alerts On Metrics Service is alive.')
-  setTimeout(sendIamAliveMessages, 1000 * IAM_ALIVE_MESSAGE_INTERVAL);
+  logInfo('[SERVICE HEALTH] Alerts On Metrics Service is alive.');
+  setTimeout(
+    sendIamAliveMessages,
+    1000 * (config.timing.iamAliveMessageInterval as unknown as number),
+  );
 };
