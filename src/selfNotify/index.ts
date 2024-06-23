a4c023cf075960b599329ba0dc8a2f81afe3015c
Jakub Parapura <jakubparapura@gmail.com>
2024-06-23 14:17:27 +0200

    :necktie: Send service-is-alive messages to separate discord channel (#32)

--- src/selfNotify/index.ts
+++ src/selfNotify/index.ts
@@ -1,10 +1,27 @@
-import { logInfo } from '../basicService/logging/applicationLogging';
 import config from '../config/runtime';
+import { MessageType } from '../mediums/interfaces';
+import mediumsManager from '../mediums/mediumsManager';
 
 export const sendIamAliveMessages = () => {
-  logInfo('[SERVICE HEALTH] Alerts On Metrics Service is alive.');
+  // TODO: This is absolutely pathetic way to handle scenario when on service startup
+  // we try to send service-alive message but there are mediums (like Discord) that
+  // are not yet ready to receive requests.
+  setTimeout(() => postponeInitialMessage(), 5000);
+};
+
+const postponeInitialMessage = () => {
+  const output = generateServiceAliveMessage();
+  mediumsManager.notifyAllMediums(MessageType.serviceAlive, output);
   setTimeout(
     sendIamAliveMessages,
     1000 * (config.timing.iamAliveMessageInterval as unknown as number),
   );
+};
+
+const generateServiceAliveMessage = () => {
+  return `**Alert on Metrics Service Status**
+**Check output:**
+\`\`\`
+Service is alive.
+\`\`\``;
 };
